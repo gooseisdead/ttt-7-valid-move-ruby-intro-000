9d1015880236e4e6c9e186b8842eabce78748fd5
Mike Smith <gooseisdead@mac.com>
2020-08-02 16:10:47 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if index.between?(0, 8)
     return true
-  else (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+  else (board[index] == "X") || (board[index] == "O")
      return false
   end
 end
