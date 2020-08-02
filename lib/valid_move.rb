a5a1e6a35b16e63058441c29f9c64bcb5276d0f7
Mike Smith <gooseisdead@mac.com>
2020-08-02 16:07:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,9 +7,9 @@ end
 
 def position_taken?(board, index)
    if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-      return false
-   else
       return true
+   else
+      return false
    end
 end
 
